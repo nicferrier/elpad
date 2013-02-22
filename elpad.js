e0bf01681cf8dd2ea280657dcc95388fb7d7f217
Nic Ferier <nic@ferrier.me.uk>
2013-02-22 12:37:35 +0000

    adding bootstrap and js

---
+++ elpad.js
@@ -0,0 +1,21 @@
+var elpad = 
+    (function () {
+         var init_pads = function () {
+             $(pads).each (
+                 function (i,e) {
+                     var li = $("#pad-list").append(
+                         "<li><a id='" + e + "' href='javascript:;'>" 
+                             + e + "</a></li>"
+                     );
+                 });
+             $("#pad-list li a").on(
+                 "click",
+                 function (evt)  { console.log("id ", evt.target.id); }
+             );
+         };
+
+         init_pads();
+         return {
+             // Functions to expose
+         };
+     })();
