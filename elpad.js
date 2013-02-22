f969fcef1dc82aafdf95168f59e7bae023ed8369
Nic Ferier <nic@ferrier.me.uk>
2013-02-22 14:36:51 +0000

    make sure we detect when we have the pads

--- elpad.js
+++ elpad.js
@@ -1,17 +1,19 @@
 var elpad = 
     (function () {
          var init_pads = function () {
-             $(pads).each (
-                 function (i,e) {
-                     var li = $("#pad-list").append(
-                         "<li><a id='" + e + "' href='javascript:;'>" 
-                             + e + "</a></li>"
-                     );
-                 });
-             $("#pad-list li a").on(
-                 "click",
-                 function (evt)  { console.log("id ", evt.target.id); }
-             );
+             if (window["pads"] !== undefined) {
+                 $(pads).each (
+                     function (i,e) {
+                         var li = $("#pad-list").append(
+                             "<li><a id='" + e + "' href='javascript:;'>" 
+                                 + e + "</a></li>"
+                         );
+                     });
+                 $("#pad-list li a").on(
+                     "click",
+                     function (evt)  { console.log("id ", evt.target.id); }
+                 );
+             }
          };
 
          init_pads();
